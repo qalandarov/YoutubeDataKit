3ad3bdf14bec3f4a584e55602e1627c649a2ed0d
islam <qalandarov@me.com>
2018-08-21 00:35:10 +0400

    Created a Framework project to encapsulate the code

---
+++ YoutubeDataKit/YoutubeDataKit.h
@@ -0,0 +1,19 @@
+//
+//  YoutubeDataKit.h
+//  YoutubeDataKit
+//
+//  Created by Islam Qalandarov on 8/20/18.
+//  Copyright © 2018 Qalandarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YoutubeDataKit.
+FOUNDATION_EXPORT double YoutubeDataKitVersionNumber;
+
+//! Project version string for YoutubeDataKit.
+FOUNDATION_EXPORT const unsigned char YoutubeDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoutubeDataKit/PublicHeader.h>
+
+
