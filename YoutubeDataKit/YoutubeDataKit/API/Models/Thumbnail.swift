16ca09fab276203f4a0f8e8c032916b77a27948c
islam <qalandarov@me.com>
2018-08-24 21:31:52 +0400

    Refactored Thumbnails

--- YoutubeDataKit/YoutubeDataKit/API/Models/Thumbnail.swift
+++ YoutubeDataKit/YoutubeDataKit/API/Models/Thumbnail.swift
@@ -8,56 +8,18 @@
 import Foundation
 
 // MARK: - Namespace
-public enum Thumbnails {}
-
-extension Thumbnails {
-    public struct Default: Codable {
-        public let url: String?
-        public let height: Int?
-        public let width: Int?
+public struct Thumbnails: Codable {
+    //swiftlint:disable colon
+    public struct Item: Codable {
+        public let url:     String
+        public let height:  Int
+        public let width:   Int
     }
-}
-
-extension Thumbnails {
-    public struct SearchList: Codable {
-        public let high: Default
-        public let `default`: Default
-        public let medium: Default
-    }
-}
-
-extension Thumbnails {
-    public struct VideoList: Codable {
-        public let high: Default
-        public let `default`: Default
-        public let medium: Default
-    }
-}
-
-extension Thumbnails {
-    public struct ActivityList: Codable {
-        public let high: Default
-        public let medium: Default
-        public let `default`: Default
-        public let standard: Default?
-        public let maxres: Default?
-    }
-}
-
-extension Thumbnails {
-    public struct PlaylistsList: Codable {
-        public let high: Default
-        public let medium: Default
-        public let `default`: Default
-        public let standard: Default
-    }
-}
-
-extension Thumbnails {
-    public struct PlaylistItemsList: Codable {
-        public let high: Default
-        public let medium: Default
-        public let `default`: Default
-        public let standard: Default
-    }
+    
+    public let `default`:   Item
+    public let medium:      Item
+    public let high:        Item
+    public let standard:    Item?
+    public let maxres:      Item?
+    //swiftlint:enable colon
 }
