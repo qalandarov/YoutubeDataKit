a09d304a2c2d268a84089b78bc9ec8045b3f3205
islam <qalandarov@me.com>
2018-08-24 06:01:50 +0400

    Simplified the `send` function and extracted out some code

--- YoutubeDataKit/YoutubeDataKit/API/ApiSession.swift
+++ YoutubeDataKit/YoutubeDataKit/API/ApiSession.swift
@@ -10,35 +10,22 @@ import Foundation
 public class ApiSession {
     
     public static let shared = ApiSession()
-    
     private init() {}
     
     public func send<T: Requestable>(_ request: T, closure: @escaping (Result<T.Response, Error>) -> Void) {
         
-        let urlRequest = request.makeURLRequest()
-        
-        let task = URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+        let task = URLSession.shared.dataTask(with: request.makeURLRequest()) { (data, _, error) in
             
-            // If the dataTask error is occured.
             if let error = error {
                 closure(.failed(ResponseError.unexpectedResponse(error)))
                 return
             }
             
-            // Decodable must have data length at least.
-            guard let data = data else {
-                closure(.failed(ResponseError.unexpectedResponse("The response is empty.")))
-                return
-            }
-            
-            // Decoding the response from `data` and handle DecodingError if occured.
             do {
-                let result = try T.Response.decoded(from: data)
-                closure(.success(result))
-            } catch let error as DecodingError {
+                let response = try T.Response.decoded(from: data)
+                closure(.success(response))
+            } catch let error {
                 closure(.failed(error))
-            } catch {
-                closure(.failed(ResponseError.unexpectedResponse(data)))
             }
         }
         
@@ -47,7 +34,11 @@ public class ApiSession {
 }
 
 public extension Decodable {
-    static func decoded(from data: Data) throws -> Self {
+    static func decoded(from data: Data?) throws -> Self {
+        guard let data = data, !data.isEmpty else {
+            throw ResponseError.unexpectedResponse("The response data is empty.")
+        }
+        
         return try JSONDecoder().decode(Self.self, from: data)
     }
 }
